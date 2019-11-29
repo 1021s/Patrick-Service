cddee307b0561564859b2623f8debda9d1717ee2
Patrick Bennett <pbennettwa@gmail.com>
2019-11-29 14:24:46 -0800

    setup bolierplate express server without routes

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.static(__dirname + '/../public/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => console.log(`Example app listening on port ${port}...`));
